a67cae2bedcf5e234db6989caa10d46671bd47fb
Victor Zuniga <vzuniga@poudrelibraries.org>
2014-10-08 12:04:50 -0600

    Cleared cached on SQL_Queries/ folder

--- SQL_Queries/checkout_data.sql
+++
@@ -1,4 +0,0 @@
-SELECT id, patron_record_id, item_record_id, items_display_order, due_gmt, 
-       loanrule_code_num, checkout_gmt, renewal_count, overdue_count, 
-       overdue_gmt, recall_gmt, ptype
-  FROM sierra_view.checkout;
